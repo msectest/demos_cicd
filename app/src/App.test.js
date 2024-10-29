805a4ba493b69c5a1ccc3ac2b37ef9e72347096b
Rubén <msectest@demo.com>
2024-10-29 05:58:36 -0700

    Subida inicial

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-const password = "Oops"
+const sword = "Oops"
 
 test('renders learn react link', () => {
   render(<App />);
