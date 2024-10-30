1723462f03c3ce3da53ac6624712f3d56b9b27b1
Rubén <msectest@demo.com>
2024-10-30 15:16:04 -0700

    Subida sesión 2

--- .github/workflows/scripts/secret_finder_v2.py
+++ .github/workflows/scripts/secret_finder_v2.py
@@ -1,10 +1,9 @@
-
 # Crear un programa que recoja por parámetro una ruta y una palabra clave y 
-# termine con error
-# en caso de que un archivo o más en el directorio contenga esa palabra clave
+# termine con error ( sys.exit(n) ) en caso de que un archivo o más en el
+# directorio contenga esa palabra clave
 #
-# Tip 1: Códigos de salida con error para github
-# Tip 2: Comando egrep de linux, método walk de la librería os, etc.
+# Tip 1: Comando egrep de linux, método walk de la librería os, etc.
+# Tip 2: Códigos de salida con error para github
 # https://docs.github.com/en/actions/sharing-automations/creating-actions/setting-exit-codes-for-actions
 
 
--- secret_finder_v2.py
+++ secret_finder_v2.py
@@ -1,4 +1,3 @@
-
 # Crear un programa que recoja por parámetro una ruta y una palabra clave y 
 # termine con error
 # en caso de que un archivo o más en el directorio contenga esa palabra clave
@@ -8,9 +7,18 @@
 # https://docs.github.com/en/actions/sharing-automations/creating-actions/setting-exit-codes-for-actions
 
 
-def main():
-    
-    print("Todavía no estoy listo!")
+import sys
+import subprocess
 
+def main():
+    contenido = sys.argv[1]
+    ruta = sys.argv[2]    
+    result = subprocess.run(['egrep', '-R', contenido, ruta], capture_output=True, text=True)
+    print(result)
+    if (result.returncode):
+        sys.exit(1)
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Uso: python secret_finder_2.py <contenido> <ruta>")
+        sys.exit(1)
     main()
